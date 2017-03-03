3615fd2defe4ff4ee9dc6a9ed11abb787f1a73af
Icaro Perseo <icaroperseo@protonmail.com>
2017-03-03 15:30:19 -0600

    Completed migration to version 1.11.2.

--- src/main/java/com/icaroperseo/helpfultweaks/HelpfulTweaks.java
+++ src/main/java/com/icaroperseo/helpfultweaks/HelpfulTweaks.java
@@ -17,7 +17,7 @@ public class HelpfulTweaks {
     public static final String MODID = "helpfultweaks";
     public static final String NAME = "Helpful Tweaks";
     public static final String VERSION = "1.6.0";
-    public static final String MCVERSION = "1.10.2";
+    public static final String MCVERSION = "1.11.2";
 
     @Mod.Instance(HelpfulTweaks.MODID)
     public static HelpfulTweaks instance;
